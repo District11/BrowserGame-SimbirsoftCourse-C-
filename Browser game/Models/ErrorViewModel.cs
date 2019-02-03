7646b0a0ae1b566b00c047183a7ac19db981cfc8
q.4da@yandex.ru <q.4da@yandex.ru>
2019-02-03 22:15:48 +0400

    Add project files.

---
+++ Browser game/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Browser_game.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
