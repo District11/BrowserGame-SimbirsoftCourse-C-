b927a80e9ed5bde8ce8ee6d912f295f2a48b44a3
q.4da@yandex.ru <q.4da@yandex.ru>
2019-02-25 18:20:21 +0400

    -//-

---
+++ Browser game/Models/Logger/FileLoggerExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Browser_game.Models.Logger
+{
+    public class FileLoggerExtensions
+    {
+    }
+}
