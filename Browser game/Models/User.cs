1edc1a0802ea9d6e1726e267b058f64e52a7a7c5
q.4da@yandex.ru <q.4da@yandex.ru>
2019-03-20 19:13:26 +0400

    --//--

--- Browser game/Models/User.cs
+++ Browser game/Models/User.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
+using Browser_game.Data;
 
 namespace Browser_game.Models
 {
     public class User : IdentityUser
     {
-        public int Year { get; set; }
+        /*public int Year { get; set; }
        override public string  Email { get; set; }
+        public string Name { get; set; }*/
     }
 }
