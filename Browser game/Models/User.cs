86366591d8448980f2de03fda4707591d2b012bf
q.4da@yandex.ru <q.4da@yandex.ru>
2019-03-20 18:43:31 +0400

    Добавлены два контроллера 1) UsersController 2) AccountController Добавлены представления

--- Browser game/Models/User.cs
+++ Browser game/Models/User.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
-namespace Browser_game.Controllers
+
+namespace Browser_game.Models
 {
-    public class User: IdentityUser
+    public class User : IdentityUser
     {
-              public int Year {get;set;}
+        public int Year { get; set; }
+       override public string  Email { get; set; }
     }
 }
