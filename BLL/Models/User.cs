f7a4a13fbdb17103548daf09bf7f9a7464fa439a
q.4da@yandex.ru <q.4da@yandex.ru>
2019-03-30 17:19:38 +0400

    перенесение Logger в Bg_DAL

---
+++ BLL/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Bg_DAL
+{ /// <summary>
+/// Класс пользователя пользователя 
+/// </summary>
+    public class User : IdentityUser
+    {/// <summary>
+    /// Возраст игрока
+    /// </summary>
+       public int Year { get; set; }
+        /// <summary>
+        /// Персональная почта пользователя
+        /// </summary>
+       override public string  Email { get; set; }
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+       public string Name { get; set; }
+    }
+}
